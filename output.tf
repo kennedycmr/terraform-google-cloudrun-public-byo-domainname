293b8d5b9a4175025dc32ccf31199078804751f9
Cameron Kennedy <cameron.kennedy@lendlease.com>
2021-08-06 18:52:57 -0400

    Updating output so return external IP and cloud run id

--- output.tf
+++ output.tf
@@ -1,12 +1,9 @@
-output "cloudrun_name" {
-  value = google_cloud_run_service.prd.name
+output "cloudrun_id" {
+  description = "Id of the Cloud Run deployment"
+  value = google_cloud_run_service.prd.id
 }
 
 output "public_ip" {
+  description = "Public IP Address of the Cloud Run deployment."
   value = google_compute_global_address.public_ip.address
 }
-
-output "ssl_policy" {
-  value = google_compute_ssl_policy.primary.self_link
-}
-
